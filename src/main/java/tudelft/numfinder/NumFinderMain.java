1664178c456e6daa44500a1c4faaf423d11ab706
MBactaeon <martin.a.bedoya@gmail.com>
2024-02-04 15:27:36 +0100

    Fixing and adding new test cases to NumFinder

--- src/main/java/tudelft/numfinder/NumFinderMain.java
+++ src/main/java/tudelft/numfinder/NumFinderMain.java
@@ -9,7 +9,23 @@ public class NumFinderMain {
         // nf.find(new int[] {4, 25, 7, 9});
 
         // this crashes
-        nf.find(new int[] {4, 3, 2, 1});
+        //Original case: OK
+        //nf.find(new int[] {4, 3, 2, 1});
+        
+        //Sub-test 1: decimal numbers -> Not controlled
+        //nf.find(new int[] {0.3, 3, 2, 1});
+        
+        //Sub-test 2: Equal nums -> Not controlled
+        //nf.find(new int[] {1, 1, 1, 1});
+        
+        //Sub-test 3: String -> Not controlled
+        //nf.find(new int[] {1, "2", 3, 4});
+
+        //Sub-test 4: Null array -> Not controlled
+        //nf.find(new int[] {});
+
+        //Sub-test 5: Operations array -> OK
+        nf.find(new int[] {1+5, 6-6, 5-4, 10+0});
 
         System.out.println(nf.getLargest());
         System.out.println(nf.getSmallest());
